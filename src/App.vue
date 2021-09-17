70c94b52dde23449960b65aa5a38cd3b3fb06564
kovlento <bluskytee@163.com>
2021-09-17 21:36:40 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hello</h1>
+  </div>
+</template>
+<script lang="ts">
+import{ defineComponent }from 'vue';
+export default defineComponent({
+  name: "",
+  setup: () => {
+
+  }
+})
+</script>
+<style lang="scss" scoped>
+
+</style>
